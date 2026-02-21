24497e9c93494be0d25dc9e6052caeb1dd4e93f7
cesarsouz <cesarsouz10@gmail.com>
2026-02-21 20:33:23 -0300

    add

---
+++ ex.python/bb.py
@@ -0,0 +1,12 @@
+import math #importa a biblioteca math para usar funções matemáticas
+
+#math pow é uma função que calcula a potência de um número.
+print("2 elevado a 5 é " + str(math.pow(2, 5)))
+
+print(math.ceil(2.3)) #math ceil é uma função que arredonda um número para cima.
+
+print(math.floor(2.3)) #math floor é uma função que arredonda um número para baixo.
+
+
+
+
