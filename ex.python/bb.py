9ce6f6ab8554d36e65f7cb44af88a2ef5f38f2cf
cesarsouz <cesarsouz10@gmail.com>
2026-02-21 20:43:34 -0300

    add

--- ex.python/bb.py
+++ ex.python/bb.py
@@ -7,6 +7,8 @@ print(math.ceil(2.3)) #math ceil é uma função que arredonda um número para c
 
 print(math.floor(2.3)) #math floor é uma função que arredonda um número para baixo.
 
+print(math.fabs(15 - 23)) #math fabs é uma função que retorna o valor absoluto de um número.
 
+print(math.log(1000, 10)) #math log é uma função que retorna o logaritmo de um número em uma base específica. Neste caso, o logaritmo de 1000 na base 10 é 3, porque 10 elevado a 3 é igual a 1000.
 
 
