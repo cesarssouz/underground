21617f321a20015821970df92d2ac67ab982dfc6
cesarsouz <cesarsouz10@gmail.com>
2026-02-21 21:02:33 -0300

    add

---
+++ ex.python/bb2.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+print(np.array([1, 2, 3]) * 3) 
+
+print(np.array([1, 2, 3]) + np.array([4, 5, 6]))
+
+u = np.array([1, 2, 3])
+n = np.array([4, 5, 6])
+
+print(np.dot(u, n)) 
